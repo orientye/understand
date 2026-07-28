9052f5542879bd193d2e20151c3cd1e2e0f5c302
orientye <orientye@qq.com>
2026-07-28 20:50:14 +0800

    ai

---
+++ s2.py
@@ -0,0 +1,4 @@
+import io
+s=open(r"d:/book/ai/build-reasoning-model-scratch.txt",encoding="utf-8").readlines()
+o="".join(s[7964:8383])
+open(r"c:/orient/my/understand/ch6b.txt","w",encoding="utf-8").write(o)
