906bb06a5860fe98f1ce7322388fcc46dbbb1271
orientye <orientye@qq.com>
2026-07-28 09:13:00 +0800

    ai

--- extract_ch3.py
+++
@@ -1,9 +0,0 @@
-import io
-with io.open(r"d:\book\ai\build-reasoning-model-scratch.txt", encoding="utf-8") as f:
-    lines = f.readlines()
-start = 2760 - 1
-end = 4291 - 1
-out = "".join(lines[start:end])
-with io.open(r"c:\orient\my\understand\ch3_part2.txt", "w", encoding="utf-8") as g:
-    g.write(out)
-print("done", len(out), "chars")
